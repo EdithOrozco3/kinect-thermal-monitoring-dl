eb5274a7e5c2905acad269cc656b31b18b32cf42
edith <edith@DESKTOP-SSUJL0A>
2026-02-23 20:58:16 -0600

    setup

---
+++ capture/kinect_stream.py
@@ -0,0 +1,32 @@
+from pykinect2 import PyKinectV2, PyKinectRuntime
+import numpy as np
+import cv2
+
+class KinectCapture:
+    def __init__(self):
+        self.kinect = PyKinectRuntime.PyKinectRuntime(
+            PyKinectV2.FrameSourceTypes_Color |
+            PyKinectV2.FrameSourceTypes_Depth |
+            PyKinectV2.FrameSourceTypes_Infrared
+        )
+
+    def get_color_frame(self):
+        if self.kinect.has_new_color_frame():
+            frame = self.kinect.get_last_color_frame()
+            return frame.reshape((1080, 1920, 4)).astype(np.uint8)
+        return None
+
+    def get_depth_frame(self):
+        if self.kinect.has_new_depth_frame():
+            frame = self.kinect.get_last_depth_frame()
+            return frame.reshape((424, 512)).astype(np.float32)
+        return None
+
+    def get_ir_frame(self):
+        if self.kinect.has_new_infrared_frame():
+            frame = self.kinect.get_last_infrared_frame()
+            return frame.reshape((424, 512)).astype(np.float32)
+        return None
+
+    def close(self):
+        self.kinect.close()
